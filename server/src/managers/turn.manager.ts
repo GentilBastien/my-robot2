5b3ff8d2aa18ec5f09a6d12a1ffebd334f5b7618
GentilBastien <bastien.ga@free.fr>
2025-12-22 22:37:27 +0100

    config server for ESM

--- server/src/managers/turn.manager.ts
+++ server/src/managers/turn.manager.ts
@@ -1,8 +1,3 @@
-import { CyclicListStructure } from '../structures/cyclic-list.structure';
-import { ComparableRobot } from 'shared';
-
 export class TurnManager {
-  constructor() {
-    const a = new CyclicListStructure<ComparableRobot>([]);
-  }
+  constructor() {}
 }
