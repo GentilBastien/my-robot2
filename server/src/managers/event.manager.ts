5164f20a81878d41a33f1cbf4de7dabec04c2798
GentilBastien <bastien.ga@free.fr>
2026-01-22 00:42:03 +0100

    game state events managers and resolvers

--- server/src/managers/event.manager.ts
+++ server/src/managers/event.manager.ts
@@ -1,18 +1,16 @@
-import { resolveGameEvent } from '../resolvers/game-events.resolver';
 import { Game } from '../states/game';
+import { resolveGameEvent } from '../resolvers/game-events.resolver';
 import { resolveRequestStateEvent } from '../resolvers/request-state-events.resolver';
-import { map } from 'rxjs';
 import { resolveResponseStateEvent } from '../resolvers/response-state-events.resolver';
+import { GameEvent } from '../events/game-event';
+import { RequestStateEvent } from '../events/request-state-event';
+import { ResponseStateEvent } from '../events/response-state-event';
 
 export class EventManager {
-  public manageGameEvent(game: Game) {
-    return game.actionEvents$.pipe(
-      map(gameEvent =>
-        resolveGameEvent(game, gameEvent).flatMap(requestEvent =>
-          resolveRequestStateEvent(game, requestEvent).flatMap(responseEvent =>
-            resolveResponseStateEvent(game, responseEvent)
-          )
-        )
+  public manageGameEvent(game: Game, gameEvent: GameEvent): void {
+    resolveGameEvent(game, gameEvent).flatMap((requestEvent: RequestStateEvent) =>
+      resolveRequestStateEvent(game, requestEvent).flatMap((responseEvent: ResponseStateEvent) =>
+        resolveResponseStateEvent(game, responseEvent)
       )
     );
   }
--- server/src/states/game.ts
+++ server/src/states/game.ts
@@ -2,18 +2,22 @@ import { GameStateTypeEnum } from 'shared';
 import { ArenaManager } from '../managers/arena.manager';
 import { TurnManager } from '../managers/turn.manager';
 import { Updatable } from './updatable';
-import { Subject } from 'rxjs';
+import { EventManager } from '../managers/event.manager';
 import { GameEvent } from '../events/game-event';
 
 export class Game implements Updatable {
   private state: GameStateTypeEnum;
 
+  private readonly eventManager = new EventManager();
   private readonly arenaManager = new ArenaManager();
   private readonly turnManager = new TurnManager();
-  public readonly actionEvents$ = new Subject<GameEvent>();
 
   constructor() {
     this.state = GameStateTypeEnum.PENDING;
+  }
+
+  public onGameEvent(gameEvent: GameEvent) {
+    this.eventManager.manageGameEvent(this, gameEvent);
   }
 
   public update(): void {
