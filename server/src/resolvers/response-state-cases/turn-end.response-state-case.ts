eb35f4a3d9d0ce65997b2d90051591ce4578d9db
BastienGattaciecca <bastien.ga@free.fr>
2026-03-03 13:28:36 +0100

    expire effect on end turn

--- server/src/resolvers/response-state-cases/turn-end.response-state-case.ts
+++ server/src/resolvers/response-state-cases/turn-end.response-state-case.ts
@@ -32,8 +32,10 @@ export function turnEndResponseStateCase(
     ...effectStatesFromCell,
   ].flatMap(effectState => {
     const effect: Effect = gameCalculator.getEffect(effectState);
+    const trigger: EffectTrigger =
+      effectState.remainingTurns <= 0 ? EffectTrigger.ON_EXPIRE : EffectTrigger.ON_TURN_END;
     return effect.handle({
-      trigger: EffectTrigger.ON_TURN_END,
+      trigger,
       effectState,
       readonlyGameState,
       gameCalculator,
