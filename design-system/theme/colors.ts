bf60cbe7477f7171d81245528fa143eb59c14a7f
BastienGattaciecca <bastien.ga@free.fr>
2026-03-11 17:17:50 +0100

    DS

---
+++ design-system/theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: '#00A8FF',
+  secondary: '#FF6B6B',
+  background: '#0B0F1A',
+  panel: '#1C2333',
+  text: '#FFFFFF',
+};
---
+++ design-system/theme/typography.ts
@@ -0,0 +1,8 @@
+export const typography = {
+  fontFamily: 'Roboto, sans-serif',
+  sizes: {
+    small: 14,
+    normal: 18,
+    large: 24,
+  },
+};
