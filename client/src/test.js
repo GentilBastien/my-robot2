e08069cfbceb6e15a59fca0f4f7ca0b70e6d4187
BastienGattaciecca <bastien.ga@free.fr>
2026-03-24 15:22:37 +0100

    client ws connection setup 2

--- client/src/test.js
+++ client/src/test.js
@@ -1,13 +1,19 @@
-const connectWsBtn = document.getElementById('connect-ws');
 const connectWsInput = document.getElementById('connect-ws-input');
+const connectWsBtn = document.getElementById('connect-ws');
 const disconnectWsBtn = document.getElementById('disconnect-ws');
-const disconnectWsInput = document.getElementById('disconnect-ws-input');
+const connectedSpan = document.getElementById('connected-span');
+const blockLoginA = document.getElementById('block-login-a');
 
 let websocket = null;
+let logged = false;
+let login = '';
 
 connectWsBtn.onclick = () => {
-  websocket = new WebSocket('ws://localhost:8080/api/v1/game?login=' + connectWsInput.value);
-  websocket.onopen = () => {};
+  login = connectWsInput.value;
+  websocket = new WebSocket('ws://localhost:8080/api/v1/game?login=' + login);
+  websocket.onopen = (a, b) => {
+    updateLogged(true, login);
+  };
   websocket.onmessage = event => {
     const message = JSON.parse(event.data);
     console.log('received:', message);
@@ -15,5 +21,21 @@ connectWsBtn.onclick = () => {
 };
 
 disconnectWsBtn.onclick = () => {
+  updateLogged(false);
   websocket.close();
 };
+
+function updateLogged(flag, login) {
+  if (flag) {
+    connectedSpan.innerHTML = 'Connected as <b>' + login + '</b>';
+    blockLoginA.style.display = 'none';
+    disconnectWsBtn.style.display = 'block';
+  } else {
+    connectedSpan.innerText = '';
+    blockLoginA.style.display = 'block';
+    disconnectWsBtn.style.display = 'none';
+  }
+  logged = flag;
+  connectWsBtn.disabled = flag;
+  disconnectWsBtn.disabled = !flag;
+}
