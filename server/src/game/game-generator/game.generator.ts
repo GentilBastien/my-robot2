0bd8cc5f66fad6575aebad497a07835f8aec2341
BastienGattaciecca <bastien.ga@free.fr>
2026-04-22 16:40:58 +0200

    refacto game generator

--- server/src/game/game-generator/game.generator.ts
+++ server/src/game/game-generator/game.generator.ts
@@ -1,4 +1,12 @@
-import { GameState, GameStateTypeEnum, RobotState, TurnStateTypeEnum } from 'shared';
+import {
+  ArenaState,
+  EffectState,
+  GameState,
+  GameStateTypeEnum,
+  RobotState,
+  TurnState,
+  TurnStateTypeEnum,
+} from 'shared';
 import { GameProposal } from '@server/proposal/game-proposal';
 import { SessionManager } from '@server/session/session.manager';
 import { Game } from '@game/game';
@@ -12,35 +20,48 @@ export class GameGenerator {
   }
 
   private createNewGame(gameProposal: GameProposal): Game {
-    const robotStates: RobotState[] = gameProposal.logins as unknown as RobotState[]; //TODO get robotStates from logins
     const gameConfig: GameConfig = {
-      initialGameState: this.defineGameState(robotStates),
+      initialGameState: this.defineGameState(gameProposal),
       mapHeight: 10,
       mapWidth: 10,
     };
     return new Game(gameConfig);
   }
 
-  private defineGameState(robotStates: RobotState[]): GameState {
-    const robots: Record<string, RobotState> = robotStates.reduce(
+  private defineGameState(gameProposal: GameProposal): GameState {
+    return {
+      state: GameStateTypeEnum.PENDING,
+      turnState: this.defineInitialTurnState(),
+      arenaState: this.defineInitialArenaState(),
+      effects: this.defineInitialEffectState(),
+      robots: this.defineRobotStates(gameProposal),
+    };
+  }
+
+  private defineRobotStates(gameProposal: GameProposal): Record<string, RobotState> {
+    const robotStates: RobotState[] = gameProposal.logins as unknown as RobotState[]; //TODO get robotStates from logins
+    return robotStates.reduce(
       (acc, curr) => {
         acc[curr.id] = curr;
         return acc;
       },
       {} as Record<string, RobotState>
     );
+  }
+
+  private defineInitialTurnState(): TurnState {
     return {
-      turnState: {
-        currentTurnNumber: 0,
-        turnStateTypeEnum: TurnStateTypeEnum.PENDING,
-        currentTurnRobotId: '',
-      },
-      state: GameStateTypeEnum.PENDING,
-      arenaState: {
-        cells: [],
-      },
-      effects: [],
-      robots,
+      currentTurnNumber: 0,
+      turnStateTypeEnum: TurnStateTypeEnum.PENDING,
+      currentTurnRobotId: '',
     };
+  }
+
+  private defineInitialArenaState(): ArenaState {
+    throw 'not implemented yet';
+  }
+
+  private defineInitialEffectState(): EffectState[] {
+    return [];
   }
 }
